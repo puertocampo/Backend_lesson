c7465c4d59d7f74adf3181e1742812e4cbf045aa
puertocampo <puertocampo@gmail.com>
2018-03-24 12:49:36 +0900

    Lesson6

--- nodejs/2_server.js
+++ nodejs/2_server.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var settings = require('./settings.js');
+console.log(settings);
 var server = http.createServer();
 server.on('request', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('hello world');
     res.end();
 });
-server.listen(8080, '0.0.0.0');//待ち受け状態の場所を指定
+server.listen(settings.port, settings.host);//待ち受け状態の場所を指定
 console.log('server listening ...');
---
+++ nodejs/settings.js
@@ -0,0 +1,3 @@
+exports.port = 8080;
+exports.host = '0.0.0.0';
+
