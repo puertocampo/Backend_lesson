3e37aa67f298e634d757508ed3c9b37ce72f5d19
puertocampo <puertocampo@gmail.com>
2018-03-26 02:24:48 +0900

    expressjs_Lesson13

---
+++ expressjs/blog/app.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    app = express(),
+    post = require('./routes/post');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+
+// routing
+app.get('/', post.index);
+/*
+app.get('/posts/new', post.new);
+app.post('posts/create', post.create);
+app.get('/posts/:id', post.show);
+app.get('/posts/:id/edit', edit);
+app.put('/posts/:id/', post.update);
+app.delete('/posts/:id', post.destroy);
+*/
+app.listen(3000);
+console.log('server listening...');
