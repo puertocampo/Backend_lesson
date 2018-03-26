4f3d713a97b1e0830965e4d8fd7355ff80aefac2
puertocampo <puertocampo@gmail.com>
2018-03-26 17:53:48 +0900

    expressjs_Lesson21

--- expressjs/blog/app.js
+++ expressjs/blog/app.js
@@ -8,16 +8,28 @@ app.set('view engine', 'ejs');
 //middleware
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(function(err, req, res, next){
+    res.send(err.message);
+});
 // app.use(express.methodOverride());
+
+//csrf対策
+// app.use(express.cookieParser());
+// app.use(express.session({secret: '3948sfGUE83D'}));
+// app.use(express.csrf());
+// app.use(function(req, res, next) {
+//     res.locals.csrftoken = req.csrfToken();
+//     next();
+// })
 
 // routing
 app.get('/', post.index);
-// app.get('/posts/new', post.new);
-// app.post('posts/create', post.create);
-app.get('/posts/:id', post.show);
-// app.get('/posts/:id/edit', edit);
-// app.put('/posts/:id/', post.update);
-// app.delete('/posts/:id', post.destroy);
+app.get('/posts/:id([0-9])+', post.show);
+app.get('/posts/new', post.new);
+app.post('/posts/create', post.create);
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id', post.update);
+app.delete('/posts/:id', post.destroy);
 
 app.listen(3000);
 console.log('server listening...');
--- expressjs/blog/routes/post.js
+++ expressjs/blog/routes/post.js
@@ -1,14 +1,60 @@
 var posts = [
-    {title: 'title0', body: 'body0'},
-    {title: 'title1', body: 'body1'},
-    {title: 'title2', body: 'body2'},
+    { title: 'title0', body: 'body0' },
+    { title: 'title1', body: 'body1' },
+    { title: 'title2', body: 'body2' },
 ];
 
 // 他のファイルでも使える関数
-exports.index = function(req, res) {
-    res.render('posts/index', {posts: posts});
+exports.index = function (req, res) {
+    res.render('posts/index', { posts: posts });
 };
 
-exports.show = function(req, res) {
-    res.render('posts/show', {post: posts[req.params.id]});
+exports.show = function (req, res) {
+    res.render('posts/show', { post: posts[req.params.id] });
+};
+
+exports.new = function (req, res) {
+    res.render('posts/new');
+};
+
+exports.edit = function (req, res) {
+    res.render('posts/edit', { post: posts[req.params.id], id: req.params.id });
+};
+
+exports.update = function (req, res, next) {
+    /*
+    posts[req.body.id] = {
+        title: req.body.title,
+        body: req.body.body
+    };
+    */
+
+    if (req.body.id !== req.params.id) {
+        next(new Error('ID not valid'));
+    } else {
+        var post = {
+            title: req.body.title,
+            body: req.body.body
+        };
+        posts.push(post);
+    }
+    res.redirect('/');
+};
+
+exports.destroy = function (req, res, next) {
+    if (req.body.id !== req.params.id) {
+        next(new Error('ID not valid'));
+    } else {
+        posts.splice(req.body.id, 1);
+        res.redirect('/');
+    }
+};
+
+exports.create = function (req, res) {
+    var post = {
+        title: req.body.title,
+        body: req.body.body
+    };
+    posts.push(post);
+    res.redirect('/');
 };
