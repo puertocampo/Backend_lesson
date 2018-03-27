0be81062ff4c1b9628bc7dd94d21c36050f2a686
puertocampo <puertocampo@gmail.com>
2018-03-27 11:55:12 +0900

    更新，全検索の追加

--- mongodb/mongoose_test/ope_data.js
+++ mongodb/mongoose_test/ope_data.js
@@ -24,27 +24,40 @@ var jokigen = new Sake({
     ]
 });
 jokigen.save(function (err) {
-    if (err) {throw err;}
+    if (err) { throw err; }
 });
 
 // 検索
-Sake.find({ brand: '醸し九平次' }, function(err, result) {// brand: '醸し九平次' 
-    if (err) {throw err;}
+Sake.find({ brand: '醸し九平次' }, function (err, result) {// brand: '醸し九平次' 
+    if (err) { throw err; }
     console.log(result.brand);
     console.log(result.type);
 })
 
-Sake.findOne({ type: 9 }, function(err, result) {
-    if (err) {throw err;}
+Sake.findOne({ type: 9 }, function (err, result) {
+    if (err) { throw err; }
     console.log(result.brand);  // 登録順の関係でこちらも'醸し人九平次'が出力される
     console.log(result.type);   // '9'が出力される
-  });
+});
 
 // Population
-Sake.find({ brand: '醸し人九平次' })
-  .populate('type impressions.temperature')
-  .exec(function(err, result) {
-    if (err) {throw err;}
-    console.log(result[0].type.type);  // '純米大吟醸酒'が出力される
-    console.log(result[0].impressions[0].temperature.temperature);  // '常温'が出力される
-  });
+// Sake.find({ brand: '醸し人九平次' })
+//     .populate('type impressions.temperature')
+//     .exec(function (err, result) {
+//         if (err) { throw err; }
+//         console.log(result[0].type.type);  // '純米大吟醸酒'が出力される
+//         console.log(result[0].impressions[0].temperature.temperature);  // '常温'が出力される
+//     });
+
+Sake.update(
+    { brand: '醸し九平次' },
+    { $set: { brand: '獺祭' } },
+    function (err) {
+        if (err) throw err;
+    }
+);
+
+Sake.find({}, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+})
